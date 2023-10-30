9c7e8c27fa9ba75ee6d9a270f697e513677be694
Michreim2004 <101189072+Michreim2004@users.noreply.github.com>
2023-10-30 19:08:44 +0100

    Update MyWorld.java

--- Boulder Dash/MyWorld.java
+++ Boulder Dash/MyWorld.java
@@ -8,26 +8,30 @@ public class MyWorld extends World
     GreenfootImage s = new GreenfootImage("stone.png");
     GreenfootImage e = new GreenfootImage("ground.png");
     GreenfootImage b = new GreenfootImage("blank.png");
+    GreenfootImage g = new GreenfootImage("gold.png");
+    GreenfootImage c = new GreenfootImage("crystal.png");
+    GreenfootImage end = new GreenfootImage("portal.png");
+    GreenfootImage bot = new GreenfootImage("bot.png");
     private GreenfootImage[][] images ={
         {e,e,e,e,e,e,e,s,e,e,e,b,e,e,e,s,e,s,e,e,e,e,b,e,e,s,e,e,e},
-        {e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,s,e,e,e,e,e,b,e,e,e,b,e},
+        {e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,c,s,e,e,e,e,e,b,e,e,e,b,e},
         {e,e,e,p,e,e,e,e,e,e,e,e,e,e,e,e,s,e,e,e,e,e,e,e,e,s,e,e,e},
         {e,e,e,e,e,e,e,e,e,s,e,e,e,b,b,e,e,e,e,e,e,s,e,e,s,e,e,e,b},
         {e,e,e,e,e,e,e,e,e,s,s,e,e,s,s,s,e,e,e,s,e,e,e,e,e,s,e,s,e},
-        {s,e,e,e,b,b,e,e,e,e,e,s,e,e,e,s,b,s,e,e,e,e,e,e,e,s,e,s,s},
+        {s,e,e,e,b,b,e,e,e,e,e,s,e,e,e,s,e,s,e,e,e,e,e,e,e,s,e,s,s},
         {w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,e,e,e,e,e,s,e,e,s},
         {e,e,e,s,s,e,e,e,e,e,s,e,e,s,e,e,e,e,e,e,e,e,e,e,e,e,e,e,s},
-        {e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,s,e,s,b,b,e,e,e,e,e,e,e},
+        {e,e,e,e,c,e,e,e,e,e,e,e,e,e,e,e,e,s,e,s,c,b,e,e,e,e,e,e,e},
         {e,e,b,e,s,e,e,e,e,e,e,e,e,e,e,e,e,s,e,s,s,e,e,e,e,e,e,e,e},
-        {e,e,s,e,s,e,e,b,b,e,s,s,e,e,e,e,e,s,e,e,e,e,e,e,e,e,e,b,e},
+        {e,e,s,e,s,e,e,b,b,e,s,s,e,e,e,e,e,s,e,e,e,e,e,c,e,e,e,b,e},
         {e,e,e,s,e,e,e,e,e,e,s,e,s,e,e,e,e,e,e,e,e,e,e,s,e,e,e,s,e},
         {e,e,e,s,e,e,e,e,e,e,e,s,s,s,e,e,e,e,e,e,e,e,e,e,e,e,e,e,s},
         {b,b,e,e,e,e,e,e,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w},
-        {s,b,e,e,e,e,e,e,e,e,b,e,e,s,e,e,e,s,e,e,e,e,s,e,e,b,e,e,e},
-        {e,s,e,e,e,e,e,e,e,e,s,s,e,e,e,e,e,e,b,b,e,e,e,s,e,s,e,e,e},
-        {e,e,e,e,s,e,e,e,e,e,e,e,s,e,e,e,e,e,s,e,s,e,e,e,e,e,e,e,e},
-        {e,e,e,e,s,e,b,e,e,e,e,e,s,e,s,s,e,e,s,e,s,e,e,e,e,e,e,e,e},
-        {e,e,e,e,e,e,s,e,e,e,e,e,e,e,e,e,e,e,s,e,e,e,e,e,e,e,e,e,e},
+        {s,b,e,e,e,e,e,e,e,e,b,c,e,s,e,e,e,s,e,e,e,e,s,e,e,b,e,e,e},
+        {c,s,e,e,e,e,e,e,e,e,s,s,e,e,e,e,e,e,b,b,e,e,e,s,e,s,e,e,e},
+        {e,e,e,e,s,e,e,e,e,e,e,e,s,e,e,e,e,e,s,e,s,c,e,e,e,e,e,e,e},
+        {e,e,e,e,s,c,b,e,e,e,e,e,s,e,s,s,e,e,s,e,s,c,e,e,e,e,end,e,e},
+        {e,c,e,e,e,e,s,e,e,e,e,e,e,e,e,e,e,e,s,e,e,c,e,e,e,e,e,e,e},
     };
     
     public MyWorld()
